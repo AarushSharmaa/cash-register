682541640d2a346e84eef4349d2fa45ad86f62c5
Aarush Sharma <aarush.sharma6@gmail.com>
2022-02-15 16:03:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//visualise like input --> processing --> output 
+//input needs to be taken in javascript from HTML world
+
+
+//input is bill amount, cash given and checkbutton
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+console.log(billAmount.value);
+console.log(cashGiven.value);
+
+//we will check if this input is positive or not
+
+
